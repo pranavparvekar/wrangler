9d5b3930b76632a8ef3d4c8f0c36d5feaf9d55ec
Pranav Uday Parvekar <153606792+pranavparvekar@users.noreply.github.com>
2025-04-14 19:55:59 +0530

    Update RecipeVisitor.java

--- wrangler-core/src/main/java/io/cdap/wrangler/parser/RecipeVisitor.java
+++ wrangler-core/src/main/java/io/cdap/wrangler/parser/RecipeVisitor.java
@@ -22,6 +22,7 @@ import io.cdap.wrangler.api.SourceInfo;
 import io.cdap.wrangler.api.Triplet;
 import io.cdap.wrangler.api.parser.Bool;
 import io.cdap.wrangler.api.parser.BoolList;
+import io.cdap.wrangler.api.parser.ByteSize;
 import io.cdap.wrangler.api.parser.ColumnName;
 import io.cdap.wrangler.api.parser.ColumnNameList;
 import io.cdap.wrangler.api.parser.DirectiveName;
@@ -33,6 +34,7 @@ import io.cdap.wrangler.api.parser.Properties;
 import io.cdap.wrangler.api.parser.Ranges;
 import io.cdap.wrangler.api.parser.Text;
 import io.cdap.wrangler.api.parser.TextList;
+import io.cdap.wrangler.api.parser.TimeDuration;
 import io.cdap.wrangler.api.parser.Token;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.misc.Interval;
@@ -314,6 +316,26 @@ public final class RecipeVisitor extends DirectivesBaseVisitor<RecipeSymbol.Buil
       strs.add(text.substring(1, text.length() - 1));
     }
     builder.addToken(new TextList(strs));
+    return builder;
+  }
+
+  /**
+   * A Directive can include a byte size field (e.g., "10KB", "1.5MB"). This visitor
+   * method extracts the byte size value and creates a token type <code>ByteSize</code>.
+   */
+  @Override
+  public RecipeSymbol.Builder visitByteSizeArg(DirectivesParser.ByteSizeArgContext ctx) {
+    builder.addToken(new ByteSize(ctx.getText()));
+    return builder;
+  }
+
+  /**
+   * A Directive can include a time duration field (e.g., "150ms", "2.1s"). This visitor
+   * method extracts the time duration value and creates a token type <code>TimeDuration</code>.
+   */
+  @Override
+  public RecipeSymbol.Builder visitTimeDurationArg(DirectivesParser.ByteSizeArgContext ctx) {
+    builder.addToken(new TimeDuration(ctx.getText()));
     return builder;
   }
 
